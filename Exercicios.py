4128dbb046e9fb5e41a0e41ff4986fe01333cbb2
solxr7 <Jpsoler2008@gmail.com>
2026-03-16 11:40:38 -0300

    Aula 2 - Variaveis

---
+++ Exercicios.py
@@ -0,0 +1,17 @@
+# Exercicio 1
+'''nome = "Soler"
+
+print (F"Olá, {nome}, seja bem-vindo")'''
+
+# Exercicio 2
+'''dia = input("Digite seu dia de nascimento: ")
+mes = input("Digite seu mes de nascimento: ")
+ano = input("Digite seu ano de nascimento: ")
+
+print(f"{dia}/{mes}/{ano}")'''
+
+# Exercicio 3
+num1 = input("digite um numero: ")
+num2 = input("digite um numero: ")
+soma = (num1 + num2)
+print (f"A soma será: {soma}")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,30 @@
+print("Hello World")
+
+print(7+4)
+
+print("7 + 4")
+
+print("7"+"4")
+
+# Concatenando strigns
+
+'''
+Isso são comentários de multiplas linhas
+'''
+
+# Variaveis
+# A variavel nome recebe "Alexandre" e guarda na memoria
+nome = "Soler"
+idade = 17
+peso = 57.2
+
+print (nome, idade, peso)
+print (F"Olá, {nome}")
+
+# Input - FORMULÁRIOS  SIMPLES NO PROMPT DE COMANDO
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
